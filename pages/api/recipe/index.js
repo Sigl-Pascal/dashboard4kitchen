ca65f9bd254f8a3cd67dbe1c9da59391c048affd
Pascal Sigl <pascal@fam-sigl.de>
2022-04-06 13:04:03 +0200

    Added routes for creating and getting all users

---
+++ pages/api/recipe/index.js
@@ -0,0 +1,31 @@
+import dbConnect from '../../../config/db'
+import Recipe from '../../../models/Recipe'
+
+const handler = async (req, res) => {
+    const { method } = req
+    await dbConnect()
+
+    switch(method){
+        case'GET':
+            try {
+                const recipes = await Recipe.find({})
+                res.status(200).json({success: true, data:recipes})
+            } catch (err) {
+                res.status(400).json({success: false})
+            }
+        break
+        case'POST':
+            try {
+                const recipe = await Recipe.create(req.body)
+                res.status(200).json({success: true, data: recipe})
+            } catch (err) {
+                res.status(400).json({success: false})
+            }
+        break
+        default:
+            res.status(400).json({ success: false })
+        break
+    }
+}
+
+export default handler
