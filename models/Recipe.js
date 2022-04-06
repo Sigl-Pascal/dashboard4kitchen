4d6da4be5e867311f5013b5f7357d3ce2e389843
Pascal Sigl <pascal@fam-sigl.de>
2022-04-06 13:03:36 +0200

    Added model for recipes

---
+++ models/Recipe.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose"
+
+const IngredientSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true
+    },
+    unitOfMessure: {
+        type: String,
+        required: true
+    }
+})
+
+const RecipeSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    ingredients:{
+        type: [IngredientSchema],
+        required: true
+    },
+    rating: {
+        type: Number,
+        required: false,
+        min: 1,
+        max: 10
+    }
+})
+
+export default mongoose.models.Recipe || mongoose.model('Recipe', RecipeSchema)
