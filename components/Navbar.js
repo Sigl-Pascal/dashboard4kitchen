950bea44e78192e44c456f27cdf1ede4225d66d2
Pascal Sigl <pascal@fam-sigl.de>
2022-04-07 12:23:54 +0200

    Added Dropdown for recipes

--- components/Navbar.js
+++ components/Navbar.js
@@ -8,9 +8,23 @@ const Navbar = () => {
                     <Link href="/">
                         <a className="nav-item nav-link">Home</a>
                     </Link>
-                    <Link href="/recipes">
-                        <a className="nav-item nav-link">Rezepte</a>
-                    </Link>
+                    <div className='nav-item dropdown'>
+                        <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Rezepte
+                        </a>
+                        <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li>
+                            <Link href="/recipes">
+                                <a className="dropdown-item">Übersicht</a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/recipes/new">
+                                <a className="dropdown-item">Neues Rezept</a>
+                            </Link>
+                        </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </nav>
