e9c8e2a1f295a63d2f791a4b78646de98b4474b9
Pascal Sigl <pascal@fam-sigl.de>
2022-04-07 09:32:18 +0200

    Added Bootstrap per npm

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,6 @@ export default function Index() {
       <Head>
         <title>Dashboard</title>
         <meta name="description" content="A dashboard for use in Kitchens" />
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous"></link>
       </Head>
       <h1>Hello World!</h1>
       <div className='card' style={{width: 18 + "rem"}}>
--- pages/recipes/[id]/index.js
+++ pages/recipes/[id]/index.js
@@ -7,7 +7,6 @@ const Recipe = ({data}) => {
     <>
       <Head>
       <title>{data.name}</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous"></link>
       </Head>
           <div className="card mx-auto mt-5" style={{width: 40 + "rem"}}>
             <div className='card-header'>
