73daee118a54b05cbc280cb64b13b42f7bbcbe8b
Pascal Sigl <pascal@fam-sigl.de>
2022-04-08 11:18:01 +0200

    Changed class to className

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,14 +2,14 @@ import Link from "next/link"
 
 const Navbar = () => {
     return(
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
+        <nav className="navbar navbar-expand navbar-light bg-light">
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
                     <Link href="/">
                         <a className="nav-item nav-link">Home</a>
                     </Link>
                     <div className="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <a className="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Rezepte
                         </a>
                         <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
