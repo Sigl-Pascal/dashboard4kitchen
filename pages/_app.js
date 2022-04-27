bb79859927e5428d114d9dd447cde39268396dfe
Pascal Sigl <pascal@fam-sigl.de>
2022-04-27 19:52:52 +0200

    Added Progress Loader

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,27 @@
-import "../styles/global.css"
-import Layout from "../components/Layout"
+import '../styles/global.css'
+import Layout from '../components/Layout'
+import { useState } from 'react'
+import Router from 'next/router'
+import { LinearProgress } from '@mui/material'
 
 const App = ({ Component, pageProps }) => {
-  return <Layout><Component {...pageProps} /></Layout>
+  const [loading, setLoading] = useState(false)
+
+  Router.events.on('routeChangeStart', () => {
+    setLoading(true)
+  })
+  Router.events.on('routeChangeComplete', () => {
+    setLoading(false)
+  })
+
+  return (
+    <>
+      <Layout>
+        {loading && <LinearProgress />}
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  )
 }
-  
+
 export default App
