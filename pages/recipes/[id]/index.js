142352119d49a42c719aab1dd5f81e79ca04bef2
Pascal Sigl <pascal@fam-sigl.de>
2022-04-27 23:14:16 +0200

    Implemented recipe view page

--- pages/recipes/[id]/index.js
+++ pages/recipes/[id]/index.js
@@ -1,9 +1,185 @@
+import { Fragment } from 'react'
+import {
+  Grid,
+  Container,
+  Typography,
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  Divider,
+} from '@mui/material'
+import { DeleteOutlined, AddOutlined } from '@mui/icons-material'
+import { Formik, Form, FieldArray } from 'formik'
+import Textfield from '../../../components/FormUI/Textfield'
+import Rating from '../../../components/FormUI/Rating'
 import Head from 'next/head'
-import { Grid, Container } from '@mui/material'
-import { Formik, Form } from 'formik'
+import Link from 'next/link'
+import Router from 'next/router'
 
 const Recipe = ({ data }) => {
-  return <p>Not implemented yet</p>
+  const hanldeCancel = () => {
+    Router.back()
+  }
+
+  const INITIAL_VALUES = data
+
+  return (
+    <>
+      <Head>
+        <title>{data.name}</title>
+      </Head>
+      <Container maxWidth='md' style={{ marginTop: '10px' }}>
+        <Card>
+          <CardHeader title={<Typography variant='h3'>Rezept</Typography>} />
+          <CardContent>
+            <Formik initialValues={INITIAL_VALUES}>
+              {(props) => (
+                <Form>
+                  <Divider />
+                  <Grid container columnSpacing={1} rowSpacing={1}>
+                    <Grid item xs={12}>
+                      <Typography variant='h6'>Rezeptname</Typography>
+                    </Grid>
+                    <Grid item xs={5}>
+                      <Textfield
+                        name='name'
+                        InputProps={{
+                          readOnly: true,
+                        }}
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <Typography variant='h6'>Bewertung</Typography>
+                    </Grid>
+                    <Grid item xs={2}>
+                      <Rating name='rating' readOnly />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <Typography variant='h6'>Beschreibung</Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                      <Textfield
+                        name='description'
+                        style={{ width: '100%' }}
+                        multiline
+                        minRows={6}
+                        maxRows={6}
+                        InputProps={{
+                          readOnly: true,
+                        }}
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <Typography variant='h6'>Zutaten</Typography>
+                    </Grid>
+                    <FieldArray name='ingredients'>
+                      {(arrayHelpers) => (
+                        <Fragment>
+                          <Grid item xs={2} md={1}>
+                            <Typography>Menge</Typography>
+                          </Grid>
+                          <Grid item xs={2} md={1}>
+                            <Typography>Einheit</Typography>
+                          </Grid>
+                          <Grid item xs={6} md={9}>
+                            <Typography>Zutat</Typography>
+                          </Grid>
+                          <Grid item xs={1}>
+                            <Button
+                              type='button'
+                              disabled
+                              onClick={() =>
+                                arrayHelpers.push({
+                                  name: '',
+                                  quantity: 0,
+                                  unitOfMessure: '',
+                                })
+                              }
+                              variant='contained'
+                              color='primary'
+                            >
+                              <AddOutlined />
+                            </Button>
+                          </Grid>
+                          {props.values.ingredients.map((ingredient, index) => (
+                            <Fragment key={index}>
+                              <Grid item xs={2} md={1}>
+                                <Textfield
+                                  name={`ingredients.${index}.quantity`}
+                                  type='tel'
+                                  InputProps={{
+                                    readOnly: true,
+                                  }}
+                                />
+                              </Grid>
+                              <Grid item xs={2} md={1}>
+                                <Textfield
+                                  name={`ingredients.${index}.unitOfMessure`}
+                                  InputProps={{
+                                    readOnly: true,
+                                  }}
+                                />
+                              </Grid>
+                              <Grid item xs={5} md={9}>
+                                <Textfield
+                                  name={`ingredients.${index}.name`}
+                                  InputProps={{
+                                    readOnly: true,
+                                  }}
+                                />
+                              </Grid>
+                              <Grid
+                                item
+                                xs={1}
+                                alignContent='center'
+                                style={{ margin: 'auto' }}
+                              >
+                                <Button
+                                  type='button'
+                                  onClick={() => {
+                                    arrayHelpers.remove(index)
+                                  }}
+                                  disabled
+                                  color='error'
+                                  variant='contained'
+                                >
+                                  <DeleteOutlined />
+                                </Button>
+                              </Grid>
+                            </Fragment>
+                          ))}
+                        </Fragment>
+                      )}
+                    </FieldArray>
+                    <Grid item xs={12}>
+                      <Divider />
+                    </Grid>
+                    <Grid item xs={6} md={2}>
+                      <Link href={`/recipes/${data._id}/edit`} passHref>
+                        <Button type='button' variant='contained'>
+                          Bearbeiten
+                        </Button>
+                      </Link>
+                    </Grid>
+                    <Grid item xs={6} md={2}>
+                      <Button
+                        variant='contained'
+                        color='primary'
+                        onClick={hanldeCancel}
+                      >
+                        Zurück
+                      </Button>
+                    </Grid>
+                  </Grid>
+                </Form>
+              )}
+            </Formik>
+          </CardContent>
+        </Card>
+      </Container>
+    </>
+  )
 }
 
 export const getServerSideProps = async ({ query: { id } }) => {
--- pages/recipes/index.js
+++ pages/recipes/index.js
@@ -1,37 +1,65 @@
-import Link from "next/link"
-import Head from "next/head"
-import {Container,Grid, Card, CardContent, CardHeader, CardActions, Button, IconButton, Stack} from "@mui/material"
-import {PreviewOutlined, EditOutlined, DeleteOutlined } from "@mui/icons-material"
+import Link from 'next/link'
+import Head from 'next/head'
+import {
+  Container,
+  Grid,
+  Card,
+  CardContent,
+  CardHeader,
+  CardActions,
+  Button,
+  IconButton,
+  Stack,
+} from '@mui/material'
+import {
+  PreviewOutlined,
+  EditOutlined,
+  DeleteOutlined,
+} from '@mui/icons-material'
 
-const Recipes = ({data}) => {
-  return(
+const Recipes = ({ data }) => {
+  return (
     <>
       <Head>
         <title>Rezepte</title>
       </Head>
-      <h1 style={{textAlign: "center"}}>Rezepte</h1>
-      <Container style={{marginTop: "18px"}}>
+      <h1 style={{ textAlign: 'center' }}>Rezepte</h1>
+      <Container style={{ marginTop: '18px' }}>
         <Grid container spacing={2}>
-          {data.map(recipe => {
-            return(
-              <Grid item spacing={3} key={recipe._id} xs={12} sm={12} md={6} lg={3}>
-                <Card style={{textAlign: "center"}}>
-                  <CardHeader 
-                    title= {
+          {data.map((recipe) => {
+            return (
+              <Grid
+                item
+                spacing={3}
+                key={recipe._id}
+                xs={12}
+                sm={12}
+                md={6}
+                lg={3}
+              >
+                <Card style={{ textAlign: 'center' }}>
+                  <CardHeader
+                    title={
                       <Link href={`/recipes/${recipe._id}`} passHref>
-                        <a className="blank-link">{recipe.name}</a>
+                        <a className='blank-link'>{recipe.name}</a>
                       </Link>
                     }
                   />
-                  <CardActions style={{justifyContent: "center"}}>
-                    <Stack direction="row" spacing={2}>
-                      <Link href={`/recipes/${recipe._id}/edit`} passHref>
-                        <Button color="primary" variant="contained">
+                  <CardActions style={{ justifyContent: 'center' }}>
+                    <Stack direction='row' spacing={2}>
+                      <Link
+                        href={{
+                          pathname: `/recipes/${recipe._id}/edit`,
+                          query: recipe,
+                        }}
+                        passHref
+                      >
+                        <Button color='primary' variant='contained'>
                           <EditOutlined />
                         </Button>
                       </Link>
                       <Link href={`/recipes/${recipe._id}/delete`} passHref>
-                        <Button color="error" variant="contained">
+                        <Button color='error' variant='contained'>
                           <DeleteOutlined />
                         </Button>
                       </Link>
@@ -40,18 +68,17 @@ const Recipes = ({data}) => {
                 </Card>
               </Grid>
             )
-          })
-          }
+          })}
         </Grid>
       </Container>
     </>
   )
 }
 
-export const getServerSideProps = async() => {
-    const res = await fetch("http://localhost:3000/api/recipes")
-    const { data } = await res.json()
-    return { props: {data} }
+export const getServerSideProps = async () => {
+  const res = await fetch('http://localhost:3000/api/recipes')
+  const { data } = await res.json()
+  return { props: { data } }
 }
 
 export default Recipes
