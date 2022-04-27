e93a1e3baecf195bb4d7ab673d546e23b1d62a30
Pascal Sigl <pascal@fam-sigl.de>
2022-04-27 19:43:29 +0200

    Implemented new recipe page

--- pages/recipes/new.js
+++ pages/recipes/new.js
@@ -1,8 +1,188 @@
+import { Fragment } from 'react'
+import {
+  Grid,
+  Container,
+  Typography,
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  Divider,
+} from '@mui/material'
+import { DeleteOutlined, AddOutlined } from '@mui/icons-material'
+import { Formik, Form, FieldArray } from 'formik'
+import * as Yup from 'yup'
+import Textfield from '../../components/FormUI/Textfield'
+import Textarea from '../../components/FormUI/Textarea'
+import Rating from '../../components/FormUI/Rating'
+import Router from 'next/router'
 
 const NewRecipe = () => {
-    return(
-        <p>Not implemented yet</p>
-    )
+  const INITIAL_VALUES = {
+    name: '',
+    description: '',
+    ingredients: [
+      {
+        name: '',
+        quantity: 0,
+        unitOfMessure: '',
+      },
+    ],
+    rating: 0,
+  }
+
+  const VALIDATION_SCHEMA = Yup.object().shape({})
+
+  const handleSubmit = async (values) => {
+    await fetch('http://localhost:3000/api/recipes', {
+      body: JSON.stringify(values),
+      method: 'POST',
+    })
+    Router.push('/recipes')
+  }
+
+  const hanldeCancel = () => {
+    Router.back()
+  }
+
+  return (
+    <>
+      <Container maxWidth='md' style={{ marginTop: '10px' }}>
+        <Card>
+          <CardHeader
+            title={<Typography variant='h3'>Neues Rezept</Typography>}
+          />
+
+          <CardContent>
+            {' '}
+            <Formik
+              initialValues={INITIAL_VALUES}
+              validationSchema={VALIDATION_SCHEMA}
+              onSubmit={(values) => {
+                console.log(values)
+              }}
+            >
+              {(props) => (
+                <Form>
+                  <Divider />
+                  <Grid container columnSpacing={1} rowSpacing={1}>
+                    <Grid item xs={12}>
+                      <Typography variant='h6'>Rezeptname</Typography>
+                    </Grid>
+                    <Grid item xs={5}>
+                      <Textfield name='name' />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <Typography variant='h6'>Bewertung</Typography>
+                    </Grid>
+                    <Grid item xs={2}>
+                      <Rating name='rating' />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <Typography variant='h6'>Beschreibung</Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                      <Textarea
+                        name='description'
+                        style={{ width: '100%' }}
+                        minRows={8}
+                        maxRows={12}
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <Typography variant='h6'>Zutaten</Typography>
+                    </Grid>
+                    <FieldArray name='ingredients'>
+                      {(arrayHelpers) => (
+                        <Fragment>
+                          <Grid item xs={2} md={1}>
+                            <Typography>Menge</Typography>
+                          </Grid>
+                          <Grid item xs={2} md={1}>
+                            <Typography>Einheit</Typography>
+                          </Grid>
+                          <Grid item xs={6} md={9}>
+                            <Typography>Zutat</Typography>
+                          </Grid>
+                          <Grid item xs={1}>
+                            <Button
+                              type='button'
+                              onClick={() =>
+                                arrayHelpers.push({
+                                  name: '',
+                                  quantity: 0,
+                                  unitOfMessure: '',
+                                })
+                              }
+                              variant='contained'
+                              color='primary'
+                            >
+                              <AddOutlined />
+                            </Button>
+                          </Grid>
+                          {props.values.ingredients.map((ingredient, index) => (
+                            <Fragment key={index}>
+                              <Grid item xs={2} md={1}>
+                                <Textfield
+                                  name={`ingredients.${index}.quantity`}
+                                />
+                              </Grid>
+                              <Grid item xs={2} md={1}>
+                                <Textfield
+                                  name={`ingredients.${index}.unitOfMessure`}
+                                />
+                              </Grid>
+                              <Grid item xs={5} md={9}>
+                                <Textfield name={`ingredients.${index}.name`} />
+                              </Grid>
+                              <Grid
+                                item
+                                xs={1}
+                                alignContent='center'
+                                style={{ margin: 'auto' }}
+                              >
+                                <Button
+                                  type='button'
+                                  onClick={() => {
+                                    arrayHelpers.remove(index)
+                                  }}
+                                  color='error'
+                                  variant='contained'
+                                >
+                                  <DeleteOutlined />
+                                </Button>
+                              </Grid>
+                            </Fragment>
+                          ))}
+                        </Fragment>
+                      )}
+                    </FieldArray>
+                    <Grid item xs={12}>
+                      <Divider />
+                    </Grid>
+                    <Grid item xs={6} md={2}>
+                      <Button type='submit' variant='contained'>
+                        Erstellen
+                      </Button>
+                    </Grid>
+                    <Grid item xs={6} md={2}>
+                      <Button
+                        variant='contained'
+                        color='error'
+                        onClick={hanldeCancel}
+                      >
+                        Abbrechen
+                      </Button>
+                    </Grid>
+                  </Grid>
+                </Form>
+              )}
+            </Formik>
+          </CardContent>
+        </Card>
+      </Container>
+    </>
+  )
 }
 
 export default NewRecipe
